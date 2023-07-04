dc97c5eca53853c06025a5f66064235a9beb3273
Ethan.X.Chan <xilei.ggzz@foxmail.com>
2023-07-04 14:37:14 +0800

    first commit init files

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+document.querySelector(`.wrap`).onmouseenter = function () {
+    this.style.backgroundColor = "yellow"
+}
+document.querySelector(`.wrap`).onmouseleave = function () {
+    this.style.backgroundColor = "antiquewhite"
+}
+
